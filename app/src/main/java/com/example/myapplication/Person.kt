6d0e11169aaff96e2392082ef3f82fb7aba2d8c7
Skrade <zelpos90@gmail.com>
2025-09-17 09:34:40 +0700

    Registration form

---
+++ app/src/main/java/com/example/myapplication/Person.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import java.util.Date
+
+data class Person(
+    val name: String,
+    val gender: String,
+    val course: String,
+    val difficulty: String,
+    val date: String,
+    val zodiac: String
+)
